57f5eb42940bf50ef6735552ef8cb88a0d597a90
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-25 22:41:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,20 +2,20 @@ function chocolateBars() {
   var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 }
 
-function addElementToBeginningOfArray() {
-
+function addElementToBeginningOfArray(array, element) {
+  [element, ...array] 
 }
 
-function destructivelyAddElementToBeginningOfArray() {
-
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
 }
 
 function addElementToEndOfArray() {
-
+  [...array, element] 
 }
 
 function destructivelyAddElementToEndOfArray() {
-
+  array.push(element)
 }
 
 function accessElementInArray() {
